e03b43d434daf06755a0158f46fe0afba0ef6e41
Enzo Monaco <albomonaco@gmail.com>
2026-07-15 22:26:48 -0300

    refactor: adiciona um arquivo próprio para lidar com as validações dos argumentos

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using Settings;
 using Integration;
 using Email;
+using Validation;
 
 var json = File.ReadAllText("appsettings.json");
 var settings = JsonSerializer.Deserialize<AppSettings>(json);
@@ -10,70 +11,52 @@ decimal? sendPrice = null;
 
 GetStockPrice get = new();
 EmailSender emailSender = new();
+Validator validator = new();
 
 while (true)
 {
 
-    if (args.Length == 3)
+    validator.ValidateArgs(args);
+
+    var sellPrice = decimal.Parse(args[1]);
+    var buyPrice = decimal.Parse(args[2]);
+
+    Console.WriteLine("Consultando preço...");
+    var price = await get.GetStockPriceAsync(args[0]);
+
+    if (price == null)
     {
-        if (!decimal.TryParse(args[1], out var sellPrice))
-        {
-            Console.WriteLine("ERRO: Preço de venda inválido.");
-            Environment.Exit(0);
-
-        }
-        if (!decimal.TryParse(args[2], out var buyPrice))
-        {
-            Console.WriteLine("ERRO: Preço de compra inválido");
-            Environment.Exit(0);
-        }
-
-        Console.WriteLine("Consultando preço...");
-        var price = await get.GetStockPriceAsync(args[0]);
-
-        if (price == null)
-        {
-            Console.WriteLine("ERRO: Falha na requisição à API de preços.");
-            Environment.Exit(0);
-
-        }
-
-        Console.WriteLine($"Preço = {price}\nsendPrice = {sendPrice}");
-        if (price != sendPrice)
-        {
-            Console.WriteLine("\nAnalisando preço ao enviar o email...");
-            if (price > sellPrice)
-            {
-               
-                await emailSender.SendEmail(settings!.EmailSettings, "ALERTA: É recomendável a venda do seu ativo", $"O ativo {args[0]} está acima do preço de referência para venda. \nPreço Atual: {price}");
-                sendPrice = price;
-
-                Console.WriteLine("Email de venda enviado!");
-            }
-            else if (price < buyPrice)
-            {
-
-                await emailSender.SendEmail(settings!.EmailSettings, "ALERTA: É recomendável a compra de um ativo", $"O ativo {args[0]} está abaixo do preço de referência para compra. \nPreço Atual: {price}");
-                sendPrice = price;
-
-                Console.WriteLine("Email de compra enviado!");
-            }
-
-        }
-
-        Console.WriteLine("Tempo de espera");
-        await Task.Delay(TimeSpan.FromSeconds(10));
-    }
-    else if (args.Length == 0)
-    {
-        Console.WriteLine("ERRO: Nenhum argumento foi informado.");
+        Console.WriteLine("ERRO: Falha na requisição à API de preços.");
         Environment.Exit(0);
+
     }
-    else
+
+    Console.WriteLine($"Preço = {price}\nsendPrice = {sendPrice}");
+    if (price != sendPrice)
     {
-        Console.WriteLine($"ERRO: Quantidade inválida de argumentos ({args.Length} informado(s)).");
-        Environment.Exit(0);
+        Console.WriteLine("\nAnalisando preço ao enviar o email...");
+        if (price > sellPrice)
+        {
+
+            await emailSender.SendEmail(settings!.EmailSettings, "ALERTA: É recomendável a venda do seu ativo", $"O ativo {args[0]} está acima do preço de referência para venda. \nPreço Atual: {price}");
+            sendPrice = price;
+
+            Console.WriteLine("Email de venda enviado!");
+        }
+        else if (price < buyPrice)
+        {
+
+            await emailSender.SendEmail(settings!.EmailSettings, "ALERTA: É recomendável a compra de um ativo", $"O ativo {args[0]} está abaixo do preço de referência para compra. \nPreço Atual: {price}");
+            sendPrice = price;
+
+            Console.WriteLine("Email de compra enviado!");
+        }
+
     }
+
+    Console.WriteLine("Tempo de espera");
+    await Task.Delay(TimeSpan.FromSeconds(10));
+    
 
 }
 
---
+++ Validation.cs
@@ -0,0 +1,29 @@
+﻿namespace Validation;
+public class Validator
+{
+    public void ValidateArgs(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            Console.WriteLine("ERRO: Nenhum argumento foi informado.");
+            Environment.Exit(0);
+        }
+        else if (args.Length != 3)
+        {
+            Console.WriteLine($"ERRO: Quantidade inválida de argumentos ({args.Length} informado(s)).");
+            Environment.Exit(0);
+        }
+
+        if (!decimal.TryParse(args[1], out var sellPrice))
+        {
+            Console.WriteLine("ERRO: Preço de venda inválido.");
+            Environment.Exit(0);
+
+        }
+        if (!decimal.TryParse(args[2], out var buyPrice))
+        {
+            Console.WriteLine("ERRO: Preço de compra inválido");
+            Environment.Exit(0);
+        }
+    }
+}
