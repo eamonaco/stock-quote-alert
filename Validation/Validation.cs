df6e9b36741d86941a582b0fb523e932db75c4e2
Enzo Monaco <albomonaco@gmail.com>
2026-07-18 19:51:09 -0300

    refactor: troca Environment.exit(0) por return

--- Validation/Validation.cs
+++ Validation/Validation.cs
@@ -6,24 +6,24 @@ public class Validator : IValidation
         if (args.Length == 0)
         {
             Console.WriteLine("ERRO: Nenhum argumento foi informado.");
-            Environment.Exit(0);
+            return;
         }
         else if (args.Length != 3)
         {
             Console.WriteLine($"ERRO: Quantidade inválida de argumentos ({args.Length} informado(s)).");
-            Environment.Exit(0);
+            return;
         }
 
         if (!decimal.TryParse(args[1], out var sellPrice))
         {
             Console.WriteLine("ERRO: Preço de venda inválido.");
-            Environment.Exit(0);
+            return;
 
         }
         if (!decimal.TryParse(args[2], out var buyPrice))
         {
             Console.WriteLine("ERRO: Preço de compra inválido");
-            Environment.Exit(0);
+            return;
         }
     }
 }
